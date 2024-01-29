aa1d5759af6baeed88acd09b11b2903f13434ece
edwin2k22 <zakrochristalan@gmail.com>
2024-01-29 15:36:05 -0400

    exercice2

---
+++ devoir2-2.sql
@@ -0,0 +1,3 @@
+use library;
+-- exercice2
+select title, price from titles where title not like "SU%" or "BU%"; 
--- exercice1.sql
+++
@@ -1,3 +0,0 @@
-use library;
--- exercice1
- select title, price from titles where title like "%computer%";
